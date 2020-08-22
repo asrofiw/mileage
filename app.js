a1fdecca0aba7a4f340b28c566ec4ca242bf0edb
asrofiw <31853098+asrofiw@users.noreply.github.com>
2020-08-22 22:24:32 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let waktuAwal = "10:12:21";
 let waktuAkhir = "12:00:00";
 let kecepatanAwal = 6;
 let jarakTempuh = hitungJarak.mileage(waktuAwal,waktuAkhir,kecepatanAwal);
-let konversiToKM = Math.floor(jarakTempuh / 1000);
+let konversiToKm = hitungJarak.mileage(jarakTempuh);
 
 console.log(`Jarak yang ditempuh ${jarakTempuh} m`);
 console.log(`Konversi ke Km : ${konversiToKM} Km`)
